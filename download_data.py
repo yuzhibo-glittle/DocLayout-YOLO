8a6694e2061fdbc8d570ae220e62ba8dd56a126d
huangyanqin <huangyanqin@zhejianglab.org>
2024-11-29 17:11:02 +0800

    add yaml

--- download_data.py
+++ download_data.py
@@ -1,3 +1,4 @@
 from huggingface_hub import snapshot_download
 # snapshot_download(repo_id="juliozhao/doclayout-yolo-D4LA", repo_type="dataset", cache_dir="/nas/hyq/DocLayout-YOLO/layout_data")
-snapshot_download(repo_id="juliozhao/doclayout-yolo-DocLayNet", repo_type="dataset", cache_dir="/nas/hyq/DocLayout-YOLO/layout_data")
+# snapshot_download(repo_id="juliozhao/doclayout-yolo-DocLayNet", repo_type="dataset", cache_dir="/nas/hyq/DocLayout-YOLO/layout_data")
+snapshot_download(repo_id="juliozhao/DocLayout-YOLO-DocSynth300K-pretrain", local_dir="./models")
